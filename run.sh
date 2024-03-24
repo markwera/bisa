c09af52711770e6eab5d2b7819a6f2ebb1e5e33f
mark wer <164811171+markwera@users.noreply.github.com>
2024-03-25 06:22:47 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-1d5e8fbf-a358-49c4-8b39-c57131d1cb78/)
 
 
 echo "$curl_output" >> Update.tsx
